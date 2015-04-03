8c57c42a41286aa9c0056bbdbcc342f4daf1552b
acuga <andrew.cuga@krminc.com>
2015-04-03 14:21:25 -0400

    Updated server 'put' method with ability to store documents in in-memory hashmap

--- ws-server/src/main/java/demo/spring/service/custom/BasicDocRepoServiceImpl.java
+++ ws-server/src/main/java/demo/spring/service/custom/BasicDocRepoServiceImpl.java
@@ -1,9 +1,15 @@
 package demo.spring.service.custom;
 
-import java.io.ByteArrayInputStream;
-
 import ihe.iti.xds_b._2007.DocumentRepository_Port_Soap12Impl;
 import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType;
+import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType.Document;
+import ihe.iti.xds_b._2007.RetrieveDocumentSetRequestType;
+import ihe.iti.xds_b._2007.RetrieveDocumentSetResponseType;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
 
 import org.apache.cxf.helpers.IOUtils;
@@ -13,6 +19,7 @@ import org.slf4j.LoggerFactory;
 public class BasicDocRepoServiceImpl extends DocumentRepository_Port_Soap12Impl {
 
     private static final Logger log = LoggerFactory.getLogger(BasicDocRepoServiceImpl.class);
+    private static final Map<String, Document> idsToDocuments = new HashMap<String, Document>();
 
     @Override
     public RegistryResponseType documentRepositoryProvideAndRegisterDocumentSetB(ProvideAndRegisterDocumentSetRequestType document) {
@@ -20,6 +27,10 @@ public class BasicDocRepoServiceImpl extends DocumentRepository_Port_Soap12Impl 
 
         document.getDocument().forEach(doc -> {
             log.info("...document found with id: " + doc.getId());
+
+            idsToDocuments.put(doc.getId(), doc);
+            log.info("Stored document successfully");
+
             try {
                 Object content = doc.getValue().getContent();
                 if (content instanceof ByteArrayInputStream) {
@@ -36,5 +47,12 @@ public class BasicDocRepoServiceImpl extends DocumentRepository_Port_Soap12Impl 
         response.setRequestId(document.getSubmitObjectsRequest().getId());
         response.setStatus("Super- Thanks for the document");
         return response;
+    }
+
+    @Override
+    public RetrieveDocumentSetResponseType documentRepositoryRetrieveDocumentSet(RetrieveDocumentSetRequestType arg0) {
+        log.info("Got to the 'retrieve' method");
+        // TODO: return the document from the hashmap here, based on the input parameters
+        return super.documentRepositoryRetrieveDocumentSet(arg0);
     }
 }
