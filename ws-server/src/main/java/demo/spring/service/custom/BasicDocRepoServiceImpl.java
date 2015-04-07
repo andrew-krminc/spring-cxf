c97f3aef385226925d703668c55a110f3d1414e0
Andrew Cuga <andrew.cuga@krminc.com>
2015-04-07 13:03:14 -0400

    Removed java 1.8 features to be compatible with older machines running java 1.7

--- ws-server/src/main/java/demo/spring/service/custom/BasicDocRepoServiceImpl.java
+++ ws-server/src/main/java/demo/spring/service/custom/BasicDocRepoServiceImpl.java
@@ -7,11 +7,12 @@ import ihe.iti.xds_b._2007.RetrieveDocumentSetRequestType;
 import ihe.iti.xds_b._2007.RetrieveDocumentSetResponseType;
 
 import java.io.ByteArrayInputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
 
 import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.cxf.helpers.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,34 +20,44 @@ import org.slf4j.LoggerFactory;
 public class BasicDocRepoServiceImpl extends DocumentRepository_Port_Soap12Impl {
 
     private static final Logger log = LoggerFactory.getLogger(BasicDocRepoServiceImpl.class);
-    private static final Map<String, Document> idsToDocuments = new HashMap<String, Document>();
 
     @Override
     public RegistryResponseType documentRepositoryProvideAndRegisterDocumentSetB(ProvideAndRegisterDocumentSetRequestType document) {
         log.info("Acknowledge receipt of document!");
 
-        document.getDocument().forEach(doc -> {
+        for (Document doc : document.getDocument()) {
             log.info("...document found with id: " + doc.getId());
-
-            idsToDocuments.put(doc.getId(), doc);
-            log.info("Stored document successfully");
 
             try {
                 Object content = doc.getValue().getContent();
                 if (content instanceof ByteArrayInputStream) {
                     String contents = IOUtils.toString((ByteArrayInputStream) content);
                     log.info("Retrieved contents of file: " + contents);
+                    writeContentsAsFile(doc.getId(), contents);
                 }
             } catch (Exception e) {
                 log.error(e.getMessage(), e);
             }
             log.info(".......and " + doc.getValue());
-        });
+        }
 
         RegistryResponseType response = new RegistryResponseType();
         response.setRequestId(document.getSubmitObjectsRequest().getId());
         response.setStatus("Super- Thanks for the document");
         return response;
+    }
+
+    private void writeContentsAsFile(String docId, String contents) {
+        File temp;
+        try {
+            temp = File.createTempFile(docId, ".txt");
+            FileUtils.writeStringToFile(temp, contents, "UTF-8");
+            String path = temp.getAbsolutePath();
+            log.info("Stored file with id: " + docId + " to: " + path);
+        } catch (IOException e) {
+            log.error("Unable to write to file with id: " + docId + ": " + e.getMessage(), e);
+        }
+
     }
 
     @Override
